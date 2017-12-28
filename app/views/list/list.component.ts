8e8d1aa53318584992469cf5d192d1aac8dfb981
Marcos Barrera <marcos@softwareallies.com>
2017-12-28 13:52:10 -0600

    Added Page model, Page Service and updated List Component.

--- app/views/list/list.component.ts
+++ app/views/list/list.component.ts
@@ -1,11 +1,14 @@
 import { Component } from "@angular/core";
+import { Page } from "../../models/page";
+
 @Component({
   selector: "list",
   templateUrl: "views/list/list.component.html",
   styleUrls: ["views/list/list.component.css"]
 })
 export class ListComponent {
-
+  pages: Array<Page>;
+  
   constructor() { }
 
 }
